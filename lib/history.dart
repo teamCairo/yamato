ff6e2b206d24b6ead6cee17e12565696ea9baeee
radiata1618 <haruki.u@gmail.com>
2021-05-14 13:55:48 +0900

    Historyを一旦修正

--- lib/history.dart
+++ lib/history.dart
@@ -16,38 +16,123 @@ class History extends StatefulWidget {
 class _History extends State<History> {
   int correctType;
 
+  final outlineColor = Colors.blueAccent;
+  final primaryColor = Colors.blueAccent;
+  final headeryColor = Colors.blueAccent;
+  final backColor = Colors.white;
+  final double elev = 20;
+  final double iconsize = 35;
+  final double radius = 0;
+  final double fontSize = 18;
+
   @override
   Widget build(BuildContext context) {
     QuestionList ql = QuestionList();
     return Scaffold(
       appBar: AppBar(
-        title: Text("誤答をチェックしてください"),
-        backgroundColor: Colors.lightBlue[400],
-        actions: [],
-      ),
-      floatingActionButton: Container(
-        height: 100.0,
-        width: 100.0,
-        margin: EdgeInsets.only(bottom: 100.0),
+        title: Text("学習状況"),
+        backgroundColor: Colors.blueAccent,
+        leading: Icon(Icons.home_sharp),
+        elevation: elev,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.help),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+        bottom:PreferredSize(
+            preferredSize: Size.fromHeight(40.0),
+          child:Container(
+            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              SizedBox(
+                width: 94,
+                height: 35,
+                child: Center(
+                    child: Text(
+                      "問題番号",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color:Colors.white,
+                        fontWeight: FontWeight.w100,
+                        fontFamily: "Hiragino Sans",
+                        locale: Locale("ja", "JP"),
+                      ),
+                    )),
+              ),
+              Container(height: 20, child: VerticalDivider(color: Colors.black)),
+              SizedBox(width: 45, height: 35),
+              SizedBox(
+                width: 40,
+                height: 35,
+                child: Center(
+                    child: Text(
+                      "成績",
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w100,
+                        fontFamily: "Hiragino Sans",
+                        locale: Locale("ja", "JP"),
+                      ),
+                    )),
+              ),
+              SizedBox(
+                width: 26,
+                height: 35,
+                child: Center(child: Icon(Icons.help_outline,color:Colors.white)),
+              ),
+              SizedBox(
+                width: 40,
+                height: 35,
+              ),
+              Container(
+                  height: 20,
+                  child: VerticalDivider(color: Colors.grey, thickness: 1)),
+              SizedBox(
+                width: 70,
+                height: 35,
+                child: Center(child: Icon(Icons.help_outline,color:Colors.white)),
+              )
+            ]),
+          ),
+        )
       ),
       body: SingleChildScrollView(child: ql),
       bottomNavigationBar: Container(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(5),
         margin: EdgeInsets.all(4),
         child: SizedBox(
-          width: 280,
+          width: 250,
           height: 60,
-          child: ElevatedButton(
+          child: OutlinedButton.icon(
             onPressed: () {
               Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Question()));
             },
-            child: Text(
+            label: Text(
               "選択問題をトライ",
               style: TextStyle(
-                fontSize: 20,
+                fontSize: fontSize,
+                fontWeight: FontWeight.w100,
+                fontFamily: "Hiragino Sans",
+                locale: Locale("ja", "JP"),
               ),
             ),
+            icon: Icon(Icons.replay, size: iconsize),
+            style: OutlinedButton.styleFrom(
+                elevation: elev,
+                primary: primaryColor,
+                backgroundColor: backColor,
+                side: BorderSide(color: outlineColor, width: 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(radius),
+                )),
           ),
         ),
       ),
@@ -98,31 +183,67 @@ class _QuestionListstate extends State<QuestionList> {
 
     return Column(
       children: <Widget>[
+
         Container(
             height: 700,
             width: double.infinity,
             child: ListView.builder(
                 itemBuilder: (_, index) {
                   print("101");
-                  return Card(
-                    child: CheckboxListTile(
-                        value: false,
-                        //widget.checkedList[index],
-                        onChanged: (value) {
-                          setState(() => widget.checkedList[index] = value);
-                        },
-                        title: Text(
-                          hqList[index].questionNo +
-                              "                          ○　✕　○",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                  return Container(
+                      decoration: BoxDecoration(
+                        border: const Border(
+                          top: const BorderSide(
+                            color: Colors.black12,
+                            width: 0.7,
+                          ),
                         ),
-                        //subtitle: Text(snapshot.data[index].questionText,
-                        //  textAlign: TextAlign.right,),
-                        secondary: Icon(Icons.stop_sharp, color: Colors.blue),
-                        activeColor: Colors.blue,
-                        selected: _isCheckboxChecked,
-                        controlAffinity: ListTileControlAffinity.trailing),
-                  );
+                      ),
+                      child: Row(children: [
+                    SizedBox(
+                        height: 60,
+                        width: 90,
+                        child: Center(
+                            child: Text(
+                          hqList[index].questionNo,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w300,
+                            fontFamily: "Hiragino Sans",
+                            locale: Locale("ja", "JP"),
+                          ),
+                        ))),
+                    Container(
+                        height: 20,
+                        child: VerticalDivider(color: Colors.black)),
+                    SizedBox(
+                        height: 60,
+                        width: 50,
+                        child: Center(
+                            child: Icon(Icons.close,
+                                size: 35, color: Colors.indigo))),
+                    SizedBox(
+                        height: 60,
+                        width: 50,
+                        child: Center(
+                            child: Icon(Icons.radio_button_off,
+                                size: 35, color: Colors.red))),
+                    SizedBox(
+                        height: 60,
+                        width: 50,
+                        child: Center(
+                            child: Icon(Icons.close,
+                                size: 35, color: Colors.indigo))),
+                    Container(
+                        height: 20,
+                        child: VerticalDivider(color: Colors.black)),
+                    SizedBox(
+                        height: 60,
+                        width: 70,
+                        child: Icon(Icons.check_circle_outline_outlined,
+                            size: 35, color: Colors.blueAccent)),
+                  ]));
                 },
                 itemCount: hqList.length))
       ],
