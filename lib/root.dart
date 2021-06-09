53d3b33540de63a99720cf79688b9585baac1be4
radiata1618 <haruki.u@gmail.com>
2021-06-10 07:17:39 +0900

    データ登録処理追加

--- lib/root.dart
+++ lib/root.dart
@@ -45,6 +45,7 @@ class _RootWidgetState extends State<RootWidget> {
     final double iconsize = btnHeight/2.5;
     //TODO パラメータが一つも入力されていないときは強制的にパラメータ入力画面を出す。（入力されないと操作できない（もしくはシリアルコード入力ボタンしか使えない）ようにする。）
 
+    initialDataSet();
 
     return Scaffold(
       backgroundColor: Colors.cyan[100],
@@ -94,22 +95,7 @@ class _RootWidgetState extends State<RootWidget> {
                 }
               });
             },
-          ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Finish()),
-              );
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.play_arrow),
-            onPressed: () {
-              startStudy();
-            },
-          ),
+          )
         ],
       ),
       body:SingleChildScrollView(
@@ -302,6 +288,14 @@ class _RootWidgetState extends State<RootWidget> {
       //),
     )
     );
+  }
+
+  void initialDataSet() async{
+    List<QuestionHeader> qhl= await MyDatabase().getAllquestionheaders();
+    if(qhl.length==0){
+      DataMigrant dataMigrant = DataMigrant();
+      dataMigrant.dataReset();
+    }
   }
 
   void moveToStudyStatus() async {
